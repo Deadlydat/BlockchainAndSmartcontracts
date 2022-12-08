f83ffcbd4c78ada59abdba4ecc1fb57623bf67f0
Deadlydat <81553941+Deadlydat@users.noreply.github.com>
2022-12-08 13:15:44 +0100

    created tables and added dummydata

--- db_connection.py
+++ db_connection.py
@@ -12,26 +12,61 @@ cnxn = pyodbc.connect(
     'DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=no; UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
 
-# cursor.execute("SELECT @@version;")
-# row = cursor.fetchone()
-# while row:
-#     print(row[0])
-#     row = cursor.fetchone()
 
-
-# cursor.execute("CREATE TABLE TestTable(shares integer)")
+# user table create und füllen
+# cursor.execute("CREATE TABLE Users(user_id integer NOT NULL PRIMARY KEY, name varchar(15), user_name varchar(15), coins integer)")
 # cursor.commit()
 
-
+# count = cursor.execute("""
+# INSERT INTO Users (user_id, name, user_name, coins) VALUES (?,?,?,?)""",1,"Peter Lustig","pLustig", 4000).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
 
 # count = cursor.execute("""
-# INSERT INTO TestTable (shares)
-# VALUES (?)""",
-#                        1000000).rowcount
+# INSERT INTO Users (user_id, name, user_name, coins) VALUES (?,?,?,?)""",2,"Max Musterman","musterm", 6000).rowcount
 # cnxn.commit()
 # print('Rows inserted: ' + str(count))
 
 
-cursor.execute("SELECT* FROM TestTable;")
-row = cursor.fetchone()
-print(row[0])
+
+# cursor.execute("CREATE TABLE Team(team_id integer NOT NULL PRIMARY KEY, name varchar(25), points integer)")
+# cursor.commit()
+
+# count = cursor.execute("""
+# INSERT INTO Team (team_id, name, points) VALUES (?,?,?)""",1,"Würzburg kicker", 20000).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
+#
+# count = cursor.execute("""
+# INSERT INTO Team (team_id, name, points) VALUES (?,?,?)""",2,"TSV Grombühl", 16000).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
+
+
+# cursor.execute("CREATE TABLE Soccer_Player(soccer_player_id integer NOT NULL PRIMARY KEY, name varchar(25), team_id integer, position integer, points integer)")
+# cursor.commit()
+
+#Hier muss noch für die Position eine Zahl festgelegt werden 1=torwart etc
+# count = cursor.execute("""
+# INSERT INTO Soccer_Player (soccer_player_id, name, team_id, position, points) VALUES (?,?,?,?,?)""", 1, "Walter", 2, 1, 50).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
+#
+# count = cursor.execute("""
+# INSERT INTO Soccer_Player (soccer_player_id, name, team_id, position, points) VALUES (?,?,?,?,?)""", 2, "Dieter", 2, 3, 70).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
+#
+# count = cursor.execute("""
+# INSERT INTO Soccer_Player (soccer_player_id, name, team_id, position, points) VALUES (?,?,?,?,?)""", 3, "Helmut", 1, 1, 60).rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
+
+
+
+cursor.execute("SELECT* FROM Soccer_Player;")
+row = cursor.fetchall()
+
+for item in row:
+   print(item)
+
